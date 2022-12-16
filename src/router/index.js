b25b2a3eda34979c88da1774a5c351dd3baedc10
KassimT1 <traorekassim210@gmail.com>
2022-12-16 17:30:36 +0000

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import TodoList from '../views/todo-list.vue'
+import IssueList from '../views/IssueList.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,12 +14,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/todo-list',
+    name: 'TodoList',
+    component: TodoList
+  },
+  {
+    path: '/issue-list',
+    name: 'IssueList',
+    component: IssueList
   }
 ]
 
